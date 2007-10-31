ae516994bd991d53f4f0866ee6e91358b55235bc
Chris Williams <chris@bingosnet.co.uk>
2007-10-31 17:44:07 +0000

    Import of BINGOS/POE-Component-Client-Whois-1.11 from CPAN.
    gitpan-cpan-distribution: POE-Component-Client-Whois
    gitpan-cpan-version:      1.11
    gitpan-cpan-path:         BINGOS/POE-Component-Client-Whois-1.11.tar.gz
    gitpan-cpan-author:       BINGOS
    gitpan-cpan-maturity:     released

--- lib/POE/Component/Client/Whois.pm
+++ lib/POE/Component/Client/Whois.pm
@@ -9,7 +9,7 @@ use POE::Component::Client::Whois::TLDList;
 use POE::Component::Client::Whois::IPBlks;
 use vars qw($VERSION);
 
-$VERSION = '1.10';
+$VERSION = '1.11';
 
 sub whois {
   my $package = shift;
